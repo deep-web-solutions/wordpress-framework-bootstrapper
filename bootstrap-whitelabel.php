9e8ca9db0351997f1fb8eea3b4c84d6eac61b689
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-02-06 18:42:22 +0100

    Added temp dir instructions back to the bootstrapper

--- bootstrap-whitelabel.php
+++ bootstrap-whitelabel.php
@@ -7,6 +7,11 @@ defined( 'ABSPATH' ) || exit;
 // Make sure that the whitelabel options are defined. If not, set the defaults.
 defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_NAME' ) || define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_NAME', 'Deep Web Solutions' );
 defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_LOGO' ) || define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_LOGO', __DIR__ . '/src/assets/dws_logo.svg' );
+
+defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_NAME' ) || define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_NAME', 'deep-web-solutions' );
+defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_PATH' ) || define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_PATH', wp_get_upload_dir() ['basedir'] . DIRECTORY_SEPARATOR . constant( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_NAME' ) . DIRECTORY_SEPARATOR );
+defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_URL' ) || define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_URL', wp_get_upload_dir() ['baseurl'] . '/' . dws_wp_framework_get_temp_dir_name() . '/' );
+
 defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_SUPPORT_EMAIL' ) || define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_SUPPORT_EMAIL', 'support@deep-web-solutions.com' );
 defined( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_SUPPORT_URL' ) || define( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_SUPPORT_URL', 'https://support.deep-web-solutions.com' );
 
@@ -32,6 +37,42 @@ function dws_wp_framework_get_whitelabel_name(): string {
  */
 function dws_wp_framework_get_whitelabel_logo(): string {
 	return constant( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_WHITELABEL_LOGO' );
+}
+
+/**
+ * Returns the name of the temp directory within the context of the current plugin.
+ *
+ * @since   1.0.0
+ * @version 1.0.0
+ *
+ * @return  string
+ */
+function dws_wp_framework_get_temp_dir_name(): string {
+	return constant( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_NAME' );
+}
+
+/**
+ * Returns the path to the temp directory within the context of the current plugin.
+ *
+ * @since   1.0.0
+ * @version 1.0.0
+ *
+ * @return  string
+ */
+function dws_wp_framework_get_temp_dir_path(): string {
+	return constant( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_PATH' );
+}
+
+/**
+ * Returns the URL of the temp directory within the context of the current plugin.
+ *
+ * @since   1.0.0
+ * @version 1.0.0
+ *
+ * @return  string
+ */
+function dws_wp_framework_get_temp_dir_url(): string {
+	return constant( __NAMESPACE__ . '\DWS_WP_FRAMEWORK_TEMP_DIR_URL' );
 }
 
 /**
