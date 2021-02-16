aff2c94e094466d1d5cf9b7418f7f3797b9ea911
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2021-02-16 15:16:41 +0100

    Updated package.json + Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,72 +1,77 @@
 module.exports = function( grunt ) {
-    'use strict';
+	'use strict';
 
-    // Load all grunt tasks matching the `grunt-*` pattern
-    require( 'load-grunt-tasks' )( grunt );
+	// Load all grunt tasks matching the `grunt-*` pattern
+	require( 'load-grunt-tasks' )( grunt );
 
-    // Show elapsed time
-    require( 'time-grunt' )( grunt );
+	// Show elapsed time
+	require( 'time-grunt' )( grunt );
 
-    // Project configuration
-    grunt.initConfig({
-        package: grunt.file.readJSON( 'package.json' ),
-        dirs: {
-            lang: 'src/languages',
-            code: 'src'
-        },
-        makepot: {
-            framework: {
-                options: {
-                    cwd: '<%= dirs.code %>',
-                    domainPath: 'languages',
-                    exclude: [],
-                    potFilename: 'dws-wp-framework-bootstrapper.pot',
-                    mainFile: 'bootstrap.php',
-                    potHeaders: {
-                        'report-msgid-bugs-to': 'https://github.com/deep-web-solutions/wordpress-framework-bootstrapper/issues',
-                        'project-id-version': '<%= package.title %> <%= package.version %>'
-                    },
-                    processPot: function( pot ) {
-                        delete pot.headers['x-generator'];
+	// Project configuration
+	grunt.initConfig(
+		{
+			package : grunt.file.readJSON( 'package.json' ),
+			dirs : {
+				lang : 'src/languages',
+				code : 'src'
+			},
 
-                        // include the default value of the constant DWS_WP_FRAMEWORK_WHITELABEL_NAME
-                        pot.translations['']['DWS_WP_FRAMEWORK_WHITELABEL_NAME'] = {
-                            msgid: 'Deep Web Solutions',
-                            comments: { reference: 'bootstrap-whitelabel.php:8' },
-                            msgstr: [ '' ]
-                        };
+			makepot : {
+				dist : {
+					options : {
+						cwd             : '<%= dirs.code %>',
+						domainPath      : 'languages',
+						exclude         : [],
+						potFilename     : 'dws-wp-framework-bootstrapper.pot',
+						mainFile        : 'bootstrap.php',
+						potHeaders      : {
+							'report-msgid-bugs-to'  : 'https://github.com/deep-web-solutions/wordpress-framework-bootstrapper/issues',
+							'project-id-version'    : '<%= package.title %> <%= package.version %>',
+                            'poedit'     		    : true,
+                            'x-poedit-keywordslist' : true,
+						},
+						processPot      : function( pot ) {
+							delete pot.headers['x-generator'];
 
-                        // include the default value of the constant DWS_WP_FRAMEWORK_BOOTSTRAPPER_NAME
-                        pot.translations['']['DWS_WP_FRAMEWORK_BOOTSTRAPPER_NAME'] = {
-                            msgid: 'Deep Web Solutions: Framework Bootstrapper',
-                            comments: { reference: 'bootstrap.php:38' },
-                            msgstr: [ '' ]
-                        };
+							// include the default value of the constant DWS_WP_FRAMEWORK_WHITELABEL_NAME
+							pot.translations['']['DWS_WP_FRAMEWORK_WHITELABEL_NAME'] = {
+								msgid: 'Deep Web Solutions',
+								comments: { reference: 'bootstrap-whitelabel.php:8' },
+								msgstr: [ '' ]
+							};
 
-                        return pot;
-                    },
-                    type: 'wp-plugin',
-                    updateTimestamp: false,
-                    updatePoFiles: true
-                }
-            }
-        },
-        potomo: {
-            framework: {
-                options: {
-                    poDel: false
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= dirs.lang %>',
-                    src: ['*.po'],
-                    dest: '<%= dirs.lang %>',
-                    ext: '.mo',
-                    nonull: true
-                }]
-            }
-        }
-    });
+							// include the default value of the constant DWS_WP_FRAMEWORK_BOOTSTRAPPER_NAME
+							pot.translations['']['DWS_WP_FRAMEWORK_BOOTSTRAPPER_NAME'] = {
+								msgid: 'Deep Web Solutions: Framework Bootstrapper',
+								comments: { reference: 'bootstrap.php:38' },
+								msgstr: [ '' ]
+							};
 
-    grunt.registerTask( 'default', ['makepot', 'potomo'] );
+							return pot;
+						},
+						type            : 'wp-plugin',
+						updateTimestamp : false,
+						updatePoFiles   : true
+					}
+				}
+			},
+			potomo  : {
+				dist : {
+					options : {
+						poDel : false
+					},
+					files : [ {
+						expand: true,
+						cwd: '<%= dirs.lang %>',
+						src: [ '*.po' ],
+						dest: '<%= dirs.lang %>',
+						ext: '.mo',
+						nonull: true
+					} ]
+				}
+			}
+		}
+	);
+
+	grunt.registerTask( 'default', ['makepot', 'potomo'] );
 }
