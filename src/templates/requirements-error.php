7b216f6d856a245ad0df85ac4a40677de4633afa
Antonius Hegyes <a.hegyes@deep-web-solutions.com>
2022-01-13 14:31:16 +0100

    tweaks to the error message template

--- src/templates/requirements-error.php
+++ src/templates/requirements-error.php
@@ -3,8 +3,8 @@
  * A very early error message displayed if environment requirements are not met.
  *
  * @since   1.0.0
- * @version 1.2.0
- * @package DeepWebSolutions\WP-Framework\Bootstrapper\templates
+ * @version 1.2.1
+ * @package DeepWebSolutions\WP-Framework\Bootstrapper
  *
  * @see     dws_wp_framework_output_requirements_error
  *
@@ -18,29 +18,28 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<?php do_action( 'dws_wp_framework_requirements_error_before', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
+<?php do_action( 'dws_wp_framework/bootstrapper/requirements_error_notice/before', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
 
 <div class="error notice dws-requirements-error">
-	<?php do_action( 'dws_wp_framework_requirements_error_start', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
+	<?php do_action( 'dws_wp_framework/bootstrapper/requirements_error_notice/start', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
 
 	<p>
 		<?php
-		echo wp_kses(
+		echo wp_kses_post(
 			wp_sprintf(
 				/* translators: 1. Component name, 2. Component version */
-				__( '<strong>%1$s (%2$s)</strong> has encountered an error. Your environment doesn\'t meet all of the system requirements listed below:', 'dws-wp-framework-bootstrapper' ),
+				__( '<strong>%1$s (%2$s)</strong> has encountered an error. Your environment doesn\'t meet all the system requirements listed below:', 'dws-wp-framework-bootstrapper' ),
 				$component_name,
 				$component_version
-			),
-			array(
-				'strong' => array(),
 			)
 		);
 		?>
 	</p>
 
+	<?php do_action( 'dws_wp_framework/bootstrapper/requirements_error_list/before', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
+
 	<ul class="ul-disc">
-		<?php do_action( 'dws_wp_framework_requirements_error_list_before', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
+		<?php do_action( 'dws_wp_framework/bootstrapper/requirements_error_list/start', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
 
 		<li>
 			<strong>PHP <?php echo esc_html( $min_php_version ); ?>+</strong>
@@ -51,27 +50,23 @@ defined( 'ABSPATH' ) || exit;
 			<em><?php echo esc_html( wp_sprintf( /* translators: WordPress version */ __( 'You\'re running version %s', 'dws-wp-framework-bootstrapper' ), $GLOBALS['wp_version'] ) ); ?></em>
 		</li>
 
-		<?php do_action( 'dws_wp_framework_requirements_error_list_after', $component_name, $min_php_version, $min_wp_version, $args ); ?>
+		<?php do_action( 'dws_wp_framework/bootstrapper/requirements_error_list/end', $component_name, $min_php_version, $min_wp_version, $args ); ?>
 	</ul>
+
+	<?php do_action( 'dws_wp_framework/bootstrapper/requirements_error_list/after', $component_name, $min_php_version, $min_wp_version, $args ); ?>
 
 	<p>
 		<?php
-		echo wp_kses(
+		echo wp_kses_post(
 			__(
 				'If you need to upgrade your version of PHP you can ask your hosting company for assistance, and if you need help upgrading WordPress you can refer to <a href="https://wordpress.org/support/article/updating-wordpress/" target="_blank">the Codex</a>.',
 				'dws-wp-framework-bootstrapper'
-			),
-			array(
-				'a' => array(
-					'href'   => array(),
-					'target' => array(),
-				),
 			)
 		);
 		?>
 	</p>
 
-	<?php do_action( 'dws_wp_framework_requirements_error_end', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
+	<?php do_action( 'dws_wp_framework/bootstrapper/requirements_error_notice/end', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
 </div>
 
-<?php do_action( 'dws_wp_framework_requirements_error_after', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
+<?php do_action( 'dws_wp_framework/bootstrapper/requirements_error_notice/after', $component_name, $component_version, $min_php_version, $min_wp_version, $args ); ?>
